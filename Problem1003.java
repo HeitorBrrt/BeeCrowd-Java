3a93baf853095add413cdb0964cd739e85377f7e
HeitorBrrt <115040903+HeitorBrrt@users.noreply.github.com>
2022-10-14 15:40:58 -0300

    Update Problem1003.java

--- Problem1003.java
+++ Problem1003.java
@@ -9,3 +9,20 @@ Output
 Print the message "SOMA" with all the capital letters, 
 with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. 
 */
+
+import java.util.Scanner;
+
+public class beecrowd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("First number: ");
+        int A = input.nextInt();
+
+        System.out.print("Second number: ");
+        int B = input.nextInt();
+
+        int SOMA = A+B;
+        System.out.println("SOMA = " + SOMA);
+
+    }
+}
