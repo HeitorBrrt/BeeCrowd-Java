804c5dd1c8749feb52d177dc4c80d16e3afa1c91
HeitorBrrt <115040903+HeitorBrrt@users.noreply.github.com>
2022-10-16 19:26:32 -0300

    Update Problem1004.java

--- Problem1004.java
+++ Problem1004.java
@@ -6,3 +6,17 @@ The input file contains 2 integer numbers.
 Output
 Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
 */
+
+import java.util.Scanner;
+
+public class beecrowd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("First number: ");
+        int A = input.nextInt();
+        System.out.print("Second number: ");
+        int B = input.nextInt();
+        int PROD = A*B;
+        System.out.println("SOMA = " + PROD);
+    }
+}
